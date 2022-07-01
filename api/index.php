d221a6866f39b162bdba15691ea7439116efc2eb
Justin San <santosojustin@gmail.com>
2022-07-02 01:31:11 +1000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,13 +4,13 @@ require __DIR__ . '/inc/bootstrap.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-if(isset($uri[4]) || !isset($uri[5])){
+if(isset($uri[3]) || !isset($uri[4])){
     header("HTTP/1.1 404 Not Found");
     exit;
 }
 
 require ROOT_DIR . 'controller/userController.php';
 $objFeedController = new UserController();
-$methodName = $uri[5]; 
+$methodName = $uri[4]; 
 $objFeedController->{$methodName}();
 ?>
