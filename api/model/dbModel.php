67124b738170447143de15faa3c9a16b902d14b5
Justin San <santosojustin@gmail.com>
2022-07-02 01:27:16 +1000

    Implements models

---
+++ api/model/dbModel.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('../classes/DotEnv.class.php');
+(new DotEnv(__DIR__ . '/../../.env'))->load();
+
+class Database
+{
+    protected $conn = null;
+
+    public function __construct()
+    {
+        $this->host = getenv('DB_HOST');
+        $this->user = getenv('DB_USER');
+        $this->pass = getenv('DB_PASS');
+        $this->db = getenv('DB_NAME');
+
+        try {
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+
+            if (mysqli_connect_errno()) {
+                throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
+            }
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function select($query="", $params=array())
+    {
+        try {
+            $statement = $this->exec($query, $params);
+            $result = $statement->get_result->fetch_all(MYSQLI_ASSOC);
+            $statement->close(); 
+
+            return $result;
+
+        }catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return false;
+    }
+
+    private function exec($query="", $params=array())
+    {
+        try {
+            $statement = $this->conn->prepare($query);
+
+            if (!$statement) {
+                throw new Exception("Error: " . $this->conn->error);
+            }
+
+            if ($params) {
+                $statement->bind_param(...$params);
+            }
+
+            $statement->execute();
+
+            return $statement;
+
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
+?>
