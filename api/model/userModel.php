8b2b4a44482be9dc0f2f47de4678ac1e69a708cc
Justin San <102561570@student.swin.edu.au>
2022-07-05 02:47:10 +1000

    Update userModel.php

--- api/model/userModel.php
+++ api/model/userModel.php
@@ -5,12 +5,12 @@ class UserModel extends Database
 {
     public function getUsers()
     {
-        return $this->select("SELECT * FROM users");
+        return $this->select("SELECT * FROM users ORDER BY user_id ASC");
     }
 
     public function getUserByName($name)
     {
-        return $this->select("SELECT * FROM users WHERE username = ?", array('s', $name));
+        return $this->select("SELECT * FROM users WHERE username LIKE %?%", array('s', $name));
     }
 }
 ?>
