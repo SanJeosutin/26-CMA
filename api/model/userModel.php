cd1ea9315887d197301433f172db96a442059c5e
Justin San <santosojustin@gmail.com>
2022-08-12 21:08:21 +1000

    Update userModel.php

--- api/model/userModel.php
+++ api/model/userModel.php
@@ -4,41 +4,35 @@ require_once ROOT_DIR . '/model/dbModel.php';
 class UserModel extends Database
 {
     /**
-     * API example. Will be removed / modified in the future.   
-     */
-    public function getUsers()
-    {
-        return $this->select("SELECT * FROM users ORDER BY user_id ASC");
-    }
-
-    public function getUserByName($name)
-    {
-        return $this->select('SELECT * FROM users WHERE username LIKE ?', array('s', $name));
-    }
-
-    /**
      * User API according to Jenna's Schema.
      * Check Semester 2 Folder.
      */
-    public function getUserByFirstName($fName)
-    {
-        return $this->select('SELECT * FROM users WHERE FirstName  LIKE ?', array('s', $fName));
-    }
 
-    public function getUserByLastName($lName)
+    public function getUsers()
     {
-        return $this->select('SELECT * FROM users WHERE LastName  LIKE ?', array('s', $lName));
-    }
-
-    public function getUserByEmail($uEmail)
-    {
-        return $this->select('SELECT * FROM users WHERE Email  LIKE ?', array('s', $uEmail));
+        return $this->select("SELECT * FROM user ORDER BY user_id ASC");
     }
 
     public function getUserById($uID)
     {
-        return $this->select('SELECT * FROM users WHERE UId  LIKE ?', array('s', $uID));
+        return $this->select('SELECT * FROM user WHERE UserId LIKE ?', array('s', $uID));
     }
+
+    public function getUserByFirstName($fName)
+    {
+        return $this->select('SELECT * FROM user WHERE UserFirstName LIKE ?', array('s', $fName));
+    }
+
+    public function getUserByLastName($lName)
+    {
+        return $this->select('SELECT * FROM user WHERE UserLastName LIKE ?', array('s', $lName));
+    }
+
+    public function getUserByEmail($uEmail)
+    {
+        return $this->select('SELECT * FROM user WHERE UserEmail LIKE ?', array('s', $uEmail));
+    }
+
 
 }
 ?>
