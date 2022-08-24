ce0898b24940d28c64413c9253a4959dbdf3b6ab
Justin San <santosojustin@gmail.com>
2022-08-24 20:28:44 +1000

    Update bootstrap.php

--- api/inc/bootstrap.php
+++ api/inc/bootstrap.php
@@ -8,4 +8,6 @@ define('ROOT_DIR', __DIR__ . "/../");
 require_once ROOT_DIR . '/controller/baseController.php';
 require_once ROOT_DIR . 'model/userModel.php';
 require_once ROOT_DIR . 'model/submissionModel.php';
+require_once ROOT_DIR . 'model/conferenceModel.php';
+require_once ROOT_DIR . 'model/reviewModel.php';
 ?>
