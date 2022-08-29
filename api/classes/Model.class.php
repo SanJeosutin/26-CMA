a4ef23f09fbb5454c4574e0652ba54cf409671bb
Justin San <santosojustin@gmail.com>
2022-08-30 01:06:29 +1000

    POST Fully Implemented

--- api/classes/Model.class.php
+++ api/classes/Model.class.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 require_once ROOT_DIR . '/model/dbModel.php';
 
 class Model extends Database
 {
-    
+
     /*
      * $arg_0 = [string] table name
      * $arg_1 = [string] / [array] column name(s)
@@ -21,14 +21,27 @@ class Model extends Database
         return (new self)->select("SELECT * FROM $arg_0 WHERE $arg_1 LIKE ?", array('s', $arg_2));
     }
 
-    
+
     static protected function POST()
     {
         extract(func_get_args(), EXTR_PREFIX_ALL, "arg");
 
-        return (new self)->select("INSERT INTO $arg_0 VALUES ($arg_1)");
+        $key = array();
+        $val = array();
+        $rawData = explode('&', urldecode($arg_1));
+
+        foreach ($rawData as $data) {
+            $temp = explode('=', $data);
+            array_push($key, $temp[0]);
+            array_push($val, $temp[1]);
+        }
+        
+
+        #return (new self)->select("INSERT INTO $arg_0 VALUES ($arg_1)");
+
+        return (new self)->select("INSERT INTO $arg_0 (" . implode(', ', $key) . ") " . "VALUES ('" . implode("', '", $val) . "')");
     }
-    
+
     /*
      ! -*-*-*-*-*-*NOTICE-*-*-*-*-*-*
      -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
@@ -52,4 +65,3 @@ class Model extends Database
         return (new self)->select("DELETE FROM $arg_0 WHERE $arg_1 LIKE ?", array('s', $arg_2));
     }
 }
-?>
